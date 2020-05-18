3e7fbffe1dce5c23083fd85ac954cdf754311cdf
LivBenedicto <livia.benedicto@hotmail.com>
2020-05-18 12:03:43 -0300

    Aplicação mensagem B

--- Program.cs
+++ Program.cs
@@ -9,19 +9,23 @@ namespace F360
         static async Task Main(string[] args)
         {
             HttpClient client = new HttpClient();
-            HttpResponseMessage mensagem = await client.GetAsync("https://raw.githubusercontent.com/financas360/provas/master/klingon-textoA.txt");
-            //HttpResponseMessage mensagem = await client.GetAsync("https://raw.githubusercontent.com/financas360/provas/master/klingon-textoB.txt");
+
+            // mensagem A
+            // HttpResponseMessage mensagem = await client.GetAsync("https://raw.githubusercontent.com/financas360/provas/master/klingon-textoA.txt");
+            
+            // mensagem B
+            HttpResponseMessage mensagem = await client.GetAsync("https://raw.githubusercontent.com/financas360/provas/master/klingon-textoB.txt");
             string resposta = await mensagem.Content.ReadAsStringAsync();
             
-            // Preposicoes preposicoes = new Preposicoes();
-            // Console.WriteLine(preposicoes.ContarPreposicoes(resposta));
+            Preposicoes preposicoes = new Preposicoes();
+            Console.WriteLine(preposicoes.ContarPreposicoes(resposta));
 
-            // Verbos verbos = new Verbos();
-            // Console.WriteLine(verbos.ContadorVerbos(resposta));
-            // Console.WriteLine(verbos.ContadorVerbosPrimeiraPessoa(resposta));
+            Verbos verbos = new Verbos();
+            Console.WriteLine(verbos.ContadorVerbos(resposta));
+            Console.WriteLine(verbos.ContadorVerbosPrimeiraPessoa(resposta));
 
-            // Numeros numero = new Numeros();
-            // Console.WriteLine(numero.ConversorPalavraNumero(resposta));
+            Numeros numero = new Numeros();
+            Console.WriteLine(numero.ConversorPalavraNumero(resposta));
         }
     }
 }
